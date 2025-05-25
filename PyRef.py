a2ed35a991f56bc53b5498725b59841726159a21
goldalchemist <goldalchemist@gmx.com>
2025-05-25 08:01:03 +0100

    Update PyRef.py

--- PyRef.py
+++ PyRef.py
@@ -1724,7 +1724,7 @@ class PythonHelperGUI:
         All cache files and your personal notes are stored in:
         `{CACHE_DIR}` (your home directory's hidden PyRef cache folder).
 
-        **Author:** The PyRef Developer
+        **Author:** Andrew Stephens (goldalchemist)
         **Version:** 1.0
         **License:** MIT License
 
